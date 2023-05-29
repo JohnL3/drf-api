0abb9ffd57e6b7d6dcd8c3116a8dca2a93add149
JohnL3 <contactjohnlong@gmail.com>
2023-05-29 11:44:18 +0000

    updated file

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import re
+# import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -154,6 +154,7 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+    print('******* Connected ******')
 
 
 # Password validation
